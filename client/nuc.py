88766d4ea78931aa2352d897bf7b8ec9840ded43
jamesrosstwo <james.rosstwo@gmail.com>
2025-09-17 22:05:33 -0400

    test

--- client/nuc.py
+++ client/nuc.py
@@ -89,3 +89,84 @@ class NUCInterface:
     def close(self):
         # self._nuc_client.close()
         pass
+
+
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+_REALSENSE_INTRINSICS = np.asarray([
+    [900, 0, 350],
+    [0, 900, 350],
+    [0, 0, 1]
+])
+
+
+def effective_reprojection_error(
+        keypoint_position,  # World position of point when captured by cam 0
+        keypoint_velocity,  # Velocity of keypoint
+        delta_time,  # Time offset in ms between cam 0 capture and cam 1 capture
+        T_cam1_cam0,
+):
+    pos_world_t0 = keypoint_position
+    pos_world_t1 = keypoint_position + keypoint_velocity * delta_time
+
+    pos_world_t0_homogenous = np.vstack([pos_world_t0[None].T, [[1]]])
+    pos_world_t1_homogenous = np.vstack([pos_world_t1[None].T, [[1]]])
+
+    base_extr = np.eye(4)  # we only really care about the relative offset between cam0 and 1
+
+    P_cam0 = _REALSENSE_INTRINSICS @ base_extr[:3, :]
+    P_cam1 = _REALSENSE_INTRINSICS @ (T_cam1_cam0 @ base_extr)[:3, :]
+
+    pos_cam0_t0 = P_cam0 @ pos_world_t0_homogenous
+    pos_cam0_t1 = P_cam0 @ pos_world_t1_homogenous
+
+    pos_cam1_t0 = P_cam1 @ pos_world_t0_homogenous
+    pos_cam1_t1 = P_cam1 @ pos_world_t1_homogenous
+
+    uv_cam1_t0 = pos_cam1_t0[:2] / pos_cam1_t0[2]
+    uv_cam1_t1 = pos_cam1_t1[:2] / pos_cam1_t1[2]
+
+    return np.abs(uv_cam1_t0 - uv_cam1_t1).squeeze(1)
+
+
+def generate_reprojection_figure(
+        keypoint_speed: float,
+        time_offset: float,
+        n_samples: int,
+        min_keypoint_depth: float,
+        max_keypoint_depth: float
+):
+    reproj_errors = []
+    for i in range(100000):
+        T_cam = np.eye(4)
+        point_position = np.random.uniform(low=0.5, high=1.5, size=(3,))
+        # point_position = np.zeros(3)
+        point_position[:2] = 0
+        keypoint_vel = np.random.uniform(low=-1, high=1, size=(3,))  # m/s
+        keypoint_vel = keypoint_vel / np.linalg.norm(keypoint_vel) * keypoint_speed
+
+        time_offset = 0.007  # 7ms
+        reproj_error = effective_reprojection_error(
+            keypoint_position=point_position,
+            keypoint_velocity=keypoint_vel,
+            delta_time=time_offset,
+            T_cam1_cam0=T_cam  # For the time being, imagine the reproj is for two cameras in the same position.
+        )
+        reproj_errors.append(reproj_error)
+
+    errors = np.asarray(reproj_errors)
+    df = pd.DataFrame(errors, columns=["u_error", "v_error"])
+    f_error_hist = px.histogram(df.melt(), x="value", color="variable", barmode="overlay")
+    return errors, f_error_hist
+
+
+if __name__ == "__main__":
+    base_dir = Path("synchronization_study")
+    f_error_hist.write_html("reprojection_errors.html")
+
+    print(f"Reprojection errors mean:{errors.mean():.4f}, var: {errors.var():.4f}")
